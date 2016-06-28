8f47e1df6c9d500a5db8df0e152b56fc41938d3a
Biozid02 <biozid0208@gmail.com>
2016-06-28 16:53:40 +0600

    add course model funtion

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,52 @@
 class CoursesController < ApplicationController
   def index
+      @courses = Course.all
   end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def new
+      @course = Course.new
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def create
+    @course = Course.new(courses_params)
+    if @course.save
+      redirect_to @course
+    else
+      render 'new'
+    end
+
+  end
+
+  def update
+    @course = Course.find(params[:id])
+    if @course.update(courses_params)
+      redirect_to @course
+    else
+      render 'edit'
+    end
+
+  end
+
+  def destroy
+    @course = Course.find(params[:id])
+    @course.destroy
+
+    redirect_to courses_path
+
+  end
+  private
+
+    def courses_params
+      params.require(:course).permit(:title,:description,:credit)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'courses/index'
 
 
+  resources :courses
 
   root 'courses#index'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
